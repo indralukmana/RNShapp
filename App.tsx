0bda3562d87f8049c5906a882f36abf0b8899bd4
Indra Lukmana <indralukmana@live.com>
2019-11-05 20:22:22 +0700

    ✨ new: wireup redux base

--- App.tsx
+++ App.tsx
@@ -7,68 +7,23 @@
  */
 
 import React from 'react'
-import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    Text,
-    StatusBar,
-} from 'react-native'
 
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { createStore, combineReducers } from 'redux'
 
-const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    engine: {
-        position: 'absolute',
-        right: 0,
-    },
-    body: {
-        backgroundColor: Colors.white,
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-    footer: {
-        color: Colors.dark,
-        fontSize: 12,
-        fontWeight: '600',
-        padding: 4,
-        paddingRight: 12,
-        textAlign: 'right',
-    },
-})
+import { Provider } from 'react-redux'
+
+import { View } from 'react-native'
+import productsReducer from './store/reducer'
+
+const rootReducer = combineReducers({ products: productsReducer })
+
+const store = createStore(rootReducer)
 
 const App = () => {
     return (
-        <>
-            <StatusBar barStyle="dark-content" />
-            <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}
-                >
-                    <Text>Home</Text>
-                </ScrollView>
-            </SafeAreaView>
-        </>
+        <Provider store={store}>
+            <View />
+        </Provider>
     )
 }
 
